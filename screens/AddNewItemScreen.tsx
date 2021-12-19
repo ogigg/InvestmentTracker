6628fb0a2bb7d61ded56d560760635399165ef9e
Oskar Gierszewski <oskar@moodup.team>
2021-12-19 15:12:40 +0100

    Add saving data to local storage

--- screens/AddNewItemScreen.tsx
+++ screens/AddNewItemScreen.tsx
@@ -1,16 +1,79 @@
 import { Button, StyleSheet, TouchableOpacity } from 'react-native';
-import i18n from 'i18n-js';
-import { Text, View } from '../components/Themed';
+import { t } from 'i18n-js';
+import { Text, useThemeColor, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
 import React from 'react';
+import { Input } from 'react-native-elements';
+import { Controller, useForm } from 'react-hook-form';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function AddNewItem({ navigation }:  RootStackScreenProps<'AddNewItem'>) {
+    const { control, handleSubmit, formState: { errors } } = useForm();
+    const textColor = useThemeColor({ light: 'black', dark: 'white' }, 'text');
+    const onSubmit = async (form: any) => {
+        const storedItems = await AsyncStorage.getItem('items')
+        const newItemsArray = storedItems? JSON.parse(storedItems) : []
+        newItemsArray.push(form);
+        await AsyncStorage.setItem('items', JSON.stringify(newItemsArray))
+    }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{i18n.t('dashboard.welcome')}</Text>
+      <Text style={styles.title}>{t('newItem.header')}</Text>
+      <Controller
+      name="itemName"
+      control={control}
+      rules={{
+       required: true,
+      }}
+      render={({ field: { onChange, onBlur, value } }) => (
+        <Input
+            inputStyle={{ color: textColor }}
+            label={t('newItem.cryptoInput.label')}
+            placeholder={t('newItem.cryptoInput.placeholder')}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+        />  
+      )}
+    />
+      <Controller
+      name="amount"
+      control={control}
+      rules={{
+       required: true,
+      }}
+      render={({ field: { onChange, onBlur, value } }) => (
+        <Input
+            inputStyle={{ color: textColor }}
+            label={t('newItem.amountInput.label')}
+            placeholder={t('newItem.amountInput.placeholder')}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+        />  
+      )}
+    />
+      <Controller
+      name="price"
+      control={control}
+      rules={{
+       required: true,
+      }}
+      render={({ field: { onChange, onBlur, value } }) => (
+        <Input
+            inputStyle={{ color: textColor }}
+            label={t('newItem.priceInput.label')}
+            placeholder={t('newItem.priceInput.placeholder')}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+        />  
+      )}
+    />
+      
       <Button
-        onPress={() => navigation.navigate('Root')}
-        title="Go to root"
+        onPress={handleSubmit(onSubmit)}
+        title={t('newItem.save')}
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
@@ -22,6 +85,9 @@ export default function AddNewItem({ navigation }:  RootStackScreenProps<'AddNew
 }
 
 const styles = StyleSheet.create({
+    input: {
+        backgroundColor:'red'
+    },
   container: {
     flex: 1,
     alignItems: 'center',
