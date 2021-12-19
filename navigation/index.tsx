6a407c8a8353366ba30d60b860191fc5c669be40
Oskar Gierszewski <oskar@moodup.team>
2021-12-19 17:07:55 +0100

    Add header

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,6 +7,7 @@ import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { t } from 'i18n-js';
 import * as React from 'react';
 import { ColorSchemeName, Pressable } from 'react-native';
 import { View } from '../components/Themed';
@@ -40,11 +41,14 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
  */
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-function RootNavigator() {
+function RootNavigator({ }) {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="AddNewItem" component={AddNewItemScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
+      <Stack.Screen name="AddNewItem" component={AddNewItemScreen} 
+      options={{
+          headerTitle: t('newItem.header'),
+        }} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
       <Stack.Group screenOptions={{ presentation: 'modal' }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
