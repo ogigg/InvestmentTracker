f00a71f28925aee377d31d373d9914224a567750
Oskar Gierszewski <oskar@moodup.team>
2021-12-26 20:29:40 +0100

    Remove unused navigation components

--- types.tsx
+++ types.tsx
@@ -28,8 +28,6 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 
 export type RootTabParamList = {
 	Dashboard: undefined;
-	TabOne: undefined;
-	TabTwo: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
