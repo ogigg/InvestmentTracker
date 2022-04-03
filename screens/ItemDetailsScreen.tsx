7bb93bcd9290cb460fd5452af30e9fafa13175a5
Oskar Gierszewski <oskar@moodup.team>
2022-04-03 21:59:27 +0200

    Add current price section

--- screens/ItemDetailsScreen.tsx
+++ screens/ItemDetailsScreen.tsx
@@ -10,9 +10,23 @@ import { RootStackScreenProps } from '../types';
 
 export default function ItemDetails({ route, navigation }: RootStackScreenProps<'ItemDetails'>) {
 	const { item }: { item: InvestmentItem } = route.params;
+	console.log(item);
 	return (
 		<ScrollView style={{ ...styles.screenWrapper, backgroundColor: getThemeColor('background') }}>
 			<Text style={styles.title}>{item.name}</Text>
+			{item.data && (
+				<View>
+					<Text style={styles.currentPrice}>{item.data?.current_price} USD</Text>
+					<Text
+						style={{
+							...styles.priceChange,
+							color: item.data?.price_change_24h > 0 ? 'green' : 'red',
+						}}>
+						{item.data?.price_change_24h > 0 ? '+' : ''}
+						{item.data?.price_change_24h} USD ({item.data?.price_change_percentage_24h} %)
+					</Text>
+				</View>
+			)}
 			<View style={styles.graphPlaceholder}></View>
 			<Text style={styles.title}>{t('itemDetails.purchaseHistory')}</Text>
 			{item.purchases.map(purchaseHistoryItem)}
@@ -36,5 +50,14 @@ const styles = StyleSheet.create({
 		height: 250,
 		width: '100%',
 		backgroundColor: 'blue',
+	},
+	currentPrice: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	priceChange: {
+		fontSize: 14,
+		fontWeight: '600',
+		marginBottom: 14,
 	},
 });
